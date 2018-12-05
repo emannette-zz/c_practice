9692c670d1ce849e1b30f945b636583ddfaf68f2
Ethan Mannette <ethanmannette@gmail.com>
2018-12-05 00:39:30 -0700

    started over. Init again

---
+++ first_test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	// printf needs <stdio.h>
+	printf("Hello World!\n");
+
+	// Anything greater than 0 means that the program failed
+	// This can be used for error handling
+	return 0;
+}
