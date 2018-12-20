47f0a2f3328f3e9264e3474e4450dab4ff6a3f50
Ethan Mannette <ethanmannette@gmail.com>
2018-12-20 14:44:41 -0700

    Strings method will take a hard coded first and last name. Will concatenate these strings together and print Done! if the concatenation is done correctly.

--- strings.c
+++ strings.c
@@ -6,4 +6,18 @@ void string() {
 	int age = 26;
 
 	printf("%s is %d years old.\n", name, age);
+
+	char someName[100];
+	char first_name[] = "John";
+	char last_name[] = "Doe";
+
+	last_name[0] = 'B';
+	sprintf(someName, "%s %s", first_name, last_name);
+	if (strncmp(someName, "John Boe", 100) == 0) {
+		printf("Done!\n");
+	}
+	someName[0] = '\0';
+	strncat(someName, first_name, 4);
+	strncat(someName, last_name, 20);
+	printf("This name is %s\n", someName);
 }
